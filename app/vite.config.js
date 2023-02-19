3b3c2e36af22a4845b999f3ff2ddc800282ac82e
Poon <6330137821@student.chula.ac.th>
2023-02-19 13:16:03 +0700

    Add multipage config
    Update vite config to handle multipage website at build time

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        home: path.resolve(__dirname, 'src/pages/Home.jsx'),
+        productList: path.resolve(__dirname, 'src/pages/ProductList.jsx'),
+        product: path.resolve(__dirname, 'src/pages/Product.jsx'),
+        cart: path.resolve(__dirname, 'src/pages/Cart.jsx'),
+      },
+    },
+  },
+});
